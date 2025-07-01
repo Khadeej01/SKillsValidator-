ad939f1295dc8301012359cf6cdf5fe259145f13
khadijaDev <khadijaoukhlift@gmail.com>
2025-07-01 10:37:59 +0200

    Repository of competence & sousCompt

---
+++ src/main/java/org/skillsvalidator/skillsvalidator/repository/CompetenceRepository.java
@@ -0,0 +1,10 @@
+package org.skillsvalidator.skillsvalidator.repository;
+
+
+
+
+import org.skillsvalidator.skillsvalidator.model.Competence;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CompetenceRepository extends JpaRepository<Competence, Long> {
+}
