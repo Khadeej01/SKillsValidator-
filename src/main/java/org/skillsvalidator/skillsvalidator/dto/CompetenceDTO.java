4cd0de1f57654aae4c4b8d57960775748dafc053
khadijaDev <khadijaoukhlift@gmail.com>
2025-07-01 10:51:09 +0200

    DTO of compet et sousCompt

--- src/main/java/org/skillsvalidator/skillsvalidator/dto/CompetenceDTO.java
+++ src/main/java/org/skillsvalidator/skillsvalidator/dto/CompetenceDTO.java
@@ -1,4 +1,54 @@
 package org.skillsvalidator.skillsvalidator.dto;
 
+
+
+
+import java.util.List;
+
 public class CompetenceDTO {
+    private Long id;
+    private String nom;
+    private String description;
+    private List<SousCompetenceDTO> sousCompetences;
+    private boolean acquis;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public List<SousCompetenceDTO> getSousCompetences() {
+        return sousCompetences;
+    }
+
+    public void setSousCompetences(List<SousCompetenceDTO> sousCompetences) {
+        this.sousCompetences = sousCompetences;
+    }
+
+    public boolean isAcquis() {
+        return acquis;
+    }
+
+    public void setAcquis(boolean acquis) {
+        this.acquis = acquis;
+    }
 }
--- src/main/java/org/skillsvalidator/skillsvalidator/dto/SousCompetenceDTO.java
+++ src/main/java/org/skillsvalidator/skillsvalidator/dto/SousCompetenceDTO.java
@@ -1,4 +1,42 @@
 package org.skillsvalidator.skillsvalidator.dto;
 
+
+
 public class SousCompetenceDTO {
+    private Long id;
+    private String nom;
+    private String description;
+    private boolean valide;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public boolean isValide() {
+        return valide;
+    }
+
+    public void setValide(boolean valide) {
+        this.valide = valide;
+    }
 }
